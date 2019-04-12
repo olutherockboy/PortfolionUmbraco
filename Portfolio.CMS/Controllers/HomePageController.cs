84b44b88dff1c57507d21d9b1c3c40151230339e
OluAdesina <OluAdesina87@hotmail.com>
2019-04-12 12:00:09 +0100

    Added HomePage Controller Added media Files Added viewModels

---
+++ Portfolio.CMS/Controllers/HomePageController.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using Portfolio.CMS.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.PublishedContentModels;
+using System.Web.Script.Serialization;
+
+namespace Acting.Net.CMS.Controllers
+{
+    public class HomePageController : RenderMvcController
+    {
+        public ActionResult Index(HomePage model)
+
+        {
+            var ViewModel = new HomePageVM(model);
+
+            ViewModel.FullName = model.FullName != null ? model.FullName : null;
+            ViewModel.Banner = model.Banner != null ? model.Banner : null;
+            ViewModel.ProfilePicture = model.ProfilePicture != null ? model.ProfilePicture : null;
+            ViewModel.Skills = model.Skills != null ? model.Skills : null;
+            ViewModel.AboutTitle = model.AboutTitle != null ? model.AboutTitle : null;
+            ViewModel.AboutText = model.AboutText != null ? model.AboutText : null;
+
+            
+
+            ViewModel.Pictures = model.Children.Where(P => P.DocumentTypeAlias == "portfolioPictures").OfType<PortfolioPictures>().ToList();
+
+            ViewModel.Videos = model.Children.Where(v => v.DocumentTypeAlias == "showReelVideo").OfType<ShowReelVideo>().ToList();
+
+            ViewModel.Json = JsonConvert.SerializeObject(model.Skills);
+
+
+
+            return View(ViewModel);
+        }
+    }
+}
---
+++ Portfolio.CMS/ViewModels/HomePageVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Models;
+using Umbraco.Web.Models;
+using Umbraco.Web.PublishedContentModels;
+
+namespace Portfolio.CMS.ViewModels
+{
+    public class HomePageVM : RenderModel
+    {
+        public HomePageVM(IPublishedContent content) : base(content)
+        {
+        }
+
+        public string FullName { get; set; }
+        public IPublishedContent ProfilePicture { get; set; }
+        public IPublishedContent Banner { get; set; }
+        public IEnumerable<string> Skills { get; set; }
+        public string AboutText { get; set; }
+        public string AboutTitle { get; set; }
+        public string Json { get; set; }
+
+        public IEnumerable<PortfolioPictures> Pictures { get; set; }
+        public IEnumerable<ShowReelVideo> Videos { get; set; }
+
+    }
+}
